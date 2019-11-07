2135932fcf864649428c43ed27f5aebfac612977
jameshruby <windstormer4@gmail.com>
2019-11-07 19:11:01 +0100

    Removing unused code and irelevant comments

--- ConsolePiano/InstrumentalNote/NotePhases/AttackPhase.cs
+++ ConsolePiano/InstrumentalNote/NotePhases/AttackPhase.cs
@@ -7,31 +7,12 @@ namespace ConsolePiano.InstrumentalNote
 {
     class AttackPhase : Phase
     {
-        //Changing duration from direct values to 0..1 range
-
         public override double Duration => (double)1 / 20;//0.472; //(double)1 / 2;
-        //public override double Duration => 441.0;
         protected override double Strength => 32767.0;
-
-        public override double Lowerlimit { get; set; }
+        public override double Lowerlimit { get; set; } //UpperLimit => Duration
         public override double UpperLimit { get; set; }
 
-
-        //these should be fields again. as we will calculate em
-
-
-        //public override double UpperLimit => Duration;  //computed with samplesrate
-        // public AttackPhase(DefaultInstrumentNote instrument) : base(instrument) { }
-
-        /*
-            It would be NICE to precompute PhaseDuration in ctor, but at least now, 
-            we dont have durations for every phase until all the classes are created
-         */
-        public AttackPhase()
-        {
-            Lowerlimit = 0.0;
-            // UpperLimit => Duration; 
-        }
+        public AttackPhase() {}
         public AttackPhase(DefaultInstrumentNote instrument)
         {
             this.defaultInstrumentNote = instrument;
