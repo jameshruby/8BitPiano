7cf42e5632bac27fafd5ba2d1300d218abc2fd6d
jameshruby <windstormer4@gmail.com>
2019-11-09 17:12:31 +0100

    Refactoring StreamAudioBuilder for a bit

--- ConsolePiano/InstrumentalNote/StreamAudioBuilder.cs
+++ ConsolePiano/InstrumentalNote/StreamAudioBuilder.cs
@@ -6,14 +6,15 @@ namespace ConsolePiano.InstrumentalNote
 {
     public class StreamAudioBuilder
     {
+        private const int sampleRate = 44100;
+        private const int bitDepth = 16;
         //only Singleton so far
         private static StreamAudioBuilder streamAudioBuilderSingleton;
         public static StreamAudioBuilder GetInstance()
         {
             return streamAudioBuilderSingleton ?? (streamAudioBuilderSingleton = new StreamAudioBuilder());
         }
-        private StreamAudioBuilder() {}
-
+        private StreamAudioBuilder() { }
         /// <summary>
         /// Scheme : //values var is equivalent to Samples
         // attack   decay sustain  release
@@ -28,89 +29,53 @@ namespace ConsolePiano.InstrumentalNote
         /// <param name="frequency"></param>
         public int GetSampleSize(double Duration)
         {
-            var sampleRate = 441.0;
-            return (int)(sampleRate * Duration / 10.0);
+            return (int)(sampleRate * Duration / 1000.0);
         }
-
         public MemoryStream GenerateTone(List<double> preparedTone, double frequency)
         {
-            int Bytes = GetBytesForHeader(preparedTone.Count);
-            int[] headerWav = GetWavHeader(Bytes);
-            MemoryStream memoryStream = CreateStream(Bytes);
-
-            BinaryWriter BW = new BinaryWriter(memoryStream);
-
-            WriteWavHead(headerWav, BW);
-
-            double deltaFT = GetDeltaFT(frequency);
-
+            //TODO DEBUG envelope values
             File.Delete("debug.txt");
             using (StreamWriter streamwriter = new StreamWriter("debug.txt", true, System.Text.Encoding.UTF8))
-            {
-                for (int T = 0; T < preparedTone.Count; T++)
-                {
-                    streamwriter.WriteLine(preparedTone[T].ToString());
-                    Int16 convertedNote = GetFinalSamples(preparedTone[T], deltaFT, T);
-                    WriteActualToneToWriter(convertedNote, BW);
-                }
-            }
-            BW.Flush();
+            { preparedTone.ForEach(x => streamwriter.WriteLine(x.ToString())); }
 
-            SetStreamToTheBegining(memoryStream);
-
+            Int16[] rawAudioStream = GenerateRawAudioSequence(preparedTone, frequency);
+            MemoryStream memoryStream = GenerateAudioStream(rawAudioStream);
+            //DEBUG
             System.IO.File.WriteAllBytes("lastTone.wav", memoryStream.ToArray());
-#if Debug
-		  //System.IO.File.WriteAllBytes("lastTone.wav", MS.ToArray());
-#endif
             return memoryStream;
         }
-
-        private MemoryStream CreateStream(int Bytes)
+        private Int16[] GenerateRawAudioSequence(List<double> preparedTone, double frequency)
         {
-            return new MemoryStream(44 + Bytes);
+            double deltaFT = GetDeltaFT(frequency);
+            var rawAudioStream = new Int16[preparedTone.Count];
+            for (int T = 0; T < preparedTone.Count; T++)
+                rawAudioStream[T] = GetFinalSamples(preparedTone[T], deltaFT, T);
+            return rawAudioStream;
         }
 
-        private static int[] GetWavHeader(int Bytes)
+        private MemoryStream GenerateAudioStream(Int16[] rawAudioStream)
         {
-            int[] headerWav = { 0X46464952, 36 + Bytes, 0X45564157, 0X20746D66, 16, 0X20001, 44100, 176400, 0X100004, 0X61746164, Bytes };
-            return headerWav;
-        }
-        
-        private static int GetBytesForHeader(int samplesSize)
-        {
-            int Bytes = samplesSize * sizeof(int);
-            return Bytes;
-        }
+            int size = GetSize(rawAudioStream.Length);
+            MemoryStream memoryStream = CreateStream(size);
+            BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
 
-        private static void WriteWavHead(int[] headerWav, BinaryWriter BW)
-        {
-            for (int I = 0; I < headerWav.Length; I++)
-            {
-                BW.Write(headerWav[I]);
-            }
-        }
+            int[] headerWav = GetHeader(size);
+            WriteHeader(headerWav, binaryWriter);
 
-        private void SetStreamToTheBegining(MemoryStream stream)
-        {
-            stream.Seek(0, SeekOrigin.Begin);
-        }
+            foreach (var convertedNote in rawAudioStream)
+                WriteActualToneToWriter(convertedNote, binaryWriter);
 
+            binaryWriter.Flush();
+            SetStreamToTheBegining(memoryStream);
+            return memoryStream;
+        }
+        /// AUDIO STREAM
         private double GetDeltaFT(double frequency)
         {
-            var samplesPerSecond = 44100.0;
             var lenghtOfCurve = 2 * Math.PI;
-
-            var deltaFT = lenghtOfCurve * frequency / samplesPerSecond;
-
+            var deltaFT = lenghtOfCurve * frequency / sampleRate;
             return deltaFT;
         }
-
-        private void WriteActualToneToWriter(short Sample, BinaryWriter binaryWriter)
-        {
-            binaryWriter.Write(Sample);
-            binaryWriter.Write(Sample);
-        }
-
         private static short GetFinalSamples(double amplitude, double deltaFT, double T)
         {
             var freqTime = Math.Sin(deltaFT * T);
@@ -119,6 +84,36 @@ namespace ConsolePiano.InstrumentalNote
             try { convertedNote = System.Convert.ToInt16(totalnote); }
             catch (OverflowException ex) { throw new Exception("Note value out of range( " + totalnote + " )", ex); }
             return System.Convert.ToInt16(totalnote);
+        }
+        //WAV
+        private static int GetSize(int samplesSize)
+        {
+            return samplesSize * sizeof(int);
+        }
+        private MemoryStream CreateStream(int size)
+        {
+            return new MemoryStream(44 + size);
+        }
+        private static int[] GetHeader(int size)
+        {
+            int[] headerWav = { 0X46464952, 36 + size, 0X45564157, 0X20746D66, bitDepth, 0X20001, sampleRate, 176400, 0X100004, 0X61746164, size };
+            return headerWav;
+        }
+        private static void WriteHeader(int[] headerWav, BinaryWriter binaryWriter)
+        {
+            for (int I = 0; I < headerWav.Length; I++)
+            {
+                binaryWriter.Write(headerWav[I]);
+            }
+        }
+        private void WriteActualToneToWriter(short Sample, BinaryWriter binaryWriter)
+        {
+            binaryWriter.Write(Sample);
+            binaryWriter.Write(Sample);
+        }
+        private void SetStreamToTheBegining(MemoryStream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
         }
     }
 }
